d3d77ac1884afe614b82de6ccb98c6002cdd4a32
sar0868 <safarov-ar@yandex.ru>
2024-12-03 14:12:43 +0300

    add config

---
+++ .github/workflows/config.lua
@@ -0,0 +1,13 @@
+return {
+   simsim = "SECRET",
+   cf = {
+      zone = "ZONE_ID",
+      username = "sar0868",
+      api_key = "API_KEY",
+      username = "sar0868"
+   },
+   patreon = {
+      campaign = "CAMPAIGN_ID",
+      token = "ACCESS_TOKEN"
+   }
+}
